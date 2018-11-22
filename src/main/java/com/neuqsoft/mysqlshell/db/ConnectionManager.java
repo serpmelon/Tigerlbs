3d75066b073c5a2b08e4d3775ecb896854bbfb2d
taiyn <taiyn@neuqsoft.com>
2018-11-22 22:16:36 +0800

    create a new class DatabaseTest for Database; change Database class for test

--- src/main/java/com/neuqsoft/mysqlshell/db/ConnectionManager.java
+++ src/main/java/com/neuqsoft/mysqlshell/db/ConnectionManager.java
@@ -1,11 +1,31 @@
 package com.neuqsoft.mysqlshell.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class ConnectionManager {
 
 	private final String driver = "com.mysql.jdbc.Driver";
-	private final String url = "jdbc:mysql://192.168.67.233:3306/hlwjzwfw_outer_test";
-	private final String username = "hlwjzwfw";
-	private final String password = "hlwjzwfw1";
-	
-	
+	private final String url = "jdbc:mysql://127.0.0.1:3306/hlwjzwfw";
+	private final String username = "root";
+	private final String password = "123456";
+
+	public Connection getConnection() {
+
+		Connection connection = null;
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
 }
---
+++ src/test/java/com/neuqsoft/mysqlshell/db/DatabaseTest.java
@@ -0,0 +1,27 @@
+package com.neuqsoft.mysqlshell.db;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=Database.class)
+public class DatabaseTest {
+
+	@Autowired
+	private Database base;
+	@Test
+	public void queryTest() throws ClassNotFoundException, SQLException {
+
+		assertEquals("123", base.a);
+		String sql = "select * from t_user";
+		assertNotNull(base.query(sql));
+	}
+}
