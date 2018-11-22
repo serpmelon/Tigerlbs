9dd38736e10429ed035c2ed7408943ff7da925b9
serpmelon <tyn243222791@126.com>
2018-11-22 21:02:01 +0800

    test

---
+++ src/main/java/com/neuqsoft/mysqlshell/db/ConnectionManager.java
@@ -0,0 +1,11 @@
+package com.neuqsoft.mysqlshell.db;
+
+public class ConnectionManager {
+
+	private final String driver = "com.mysql.jdbc.Driver";
+	private final String url = "jdbc:mysql://192.168.67.233:3306/hlwjzwfw_outer_test";
+	private final String username = "hlwjzwfw";
+	private final String password = "hlwjzwfw1";
+	
+	
+}
